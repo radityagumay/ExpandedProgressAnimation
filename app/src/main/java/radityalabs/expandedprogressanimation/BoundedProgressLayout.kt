f8442430c8e3c7a7bcdcf38ca45ecdea514e5876
radityagumay <raditya.g@go-jek.com>
2017-09-19 08:33:50 +0530

    Fix Custom View Extended

--- app/src/main/java/radityalabs/expandedprogressanimation/BoundedProgressLayout.kt
+++ app/src/main/java/radityalabs/expandedprogressanimation/BoundedProgressLayout.kt
@@ -2,16 +2,22 @@ package radityalabs.expandedprogressanimation
 
 import android.content.Context
 import android.graphics.drawable.GradientDrawable
+import android.os.Build
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.OvershootInterpolator
 import android.widget.RelativeLayout
+import android.widget.TextView
 
 interface Action {
-    fun onLoading()
+    fun isLoading(): Boolean
 
-    fun onCompleted()
+    fun extended()
 
-    fun onProgress()
+    fun collapsed()
 
     fun setAddress(address: String)
 }
@@ -26,27 +32,70 @@ class BoundedProgressLayout @JvmOverloads constructor(
         LayoutInflater.from(context).inflate(R.layout.component_bounded_progress_layout, this, true)
     }
 
+    private var mAddress: String? = null
+
+    private var isLoading = false
+
     private lateinit var container: RelativeLayout
+    private lateinit var address: TextView
 
-    override fun onCompleted() {
+    override fun extended() {
+        isLoading = true
 
+        ViewAnimator.animate(container)
+                .dp()
+                .width(60f, 250f)
+                .interpolator(DecelerateInterpolator())
+                .duration(800)
+                .onStop {
+                    mAddress?.let {
+                        address.visibility = View.VISIBLE
+                        address.text = "Jakarta Pusat"
+                        ViewAnimator.animate(address)
+                                .dp().translationY(50f, 0f)
+                                .alpha(0.1f, 1f)
+                                .singleInterpolator(OvershootInterpolator())
+                                .duration(800)
+                                .start()
+                    }
+                }
+                .start()
+        ViewAnimator
+                .animate(container).scaleY(0f, 1f).decelerate().duration(500)
     }
 
-    override fun onLoading() {
+    override fun collapsed() {
+        isLoading = false
 
-    }
-
-    override fun onProgress() {
+        ViewAnimator.animate(address)
+                .dp().translationY(0f, 50f)
+                .alpha(1f, 0.1f)
+                .singleInterpolator(OvershootInterpolator())
+                .duration(800)
+                .onStart {
+                    ViewAnimator.animate(container)
+                            .dp()
+                            .width(250f, 60f)
+                            .interpolator(AccelerateInterpolator())
+                            .duration(1200)
+                            .start()
+                    ViewAnimator
+                            .animate(container).scaleY(1f, 0f).accelerate().duration(800)
+                }
+                .onStop { address.visibility = View.GONE }
+                .start()
     }
 
     override fun setAddress(address: String) {
+        this.mAddress = address
     }
+
+    override fun isLoading() = isLoading
 
     override fun onFinishInflate() {
         super.onFinishInflate()
         container = findViewById(R.id.container)
-
-
+        address = findViewById(R.id.address)
         initView()
     }
 
@@ -55,5 +104,9 @@ class BoundedProgressLayout @JvmOverloads constructor(
         iconBackground.cornerRadius = 100f
         iconBackground.setColor(resources.getColor(R.color.colorAccent))
         container.background = iconBackground
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            container.elevation = 4f
+        }
     }
 }
--- app/src/main/java/radityalabs/expandedprogressanimation/MainActivity.java
+++ app/src/main/java/radityalabs/expandedprogressanimation/MainActivity.java
@@ -1,7 +1,5 @@
 package radityalabs.expandedprogressanimation;
 
-import android.graphics.drawable.GradientDrawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -9,18 +7,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.DecelerateInterpolator;
-import android.view.animation.OvershootInterpolator;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RelativeLayout container;
-    private TextView textview;
-
-    private boolean isExpanded = false;
+    private BoundedProgressLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,73 +18,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        textview = (TextView) findViewById(R.id.textview);
-        container = (RelativeLayout) findViewById(R.id.container);
 
-        GradientDrawable iconBackground = new GradientDrawable();
-        iconBackground.setCornerRadius(100);
-        iconBackground.setColor(getResources().getColor(R.color.colorAccent));
-        container.setBackground(iconBackground);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            container.setElevation(4);
-        }
+        container = (BoundedProgressLayout) findViewById(R.id.container);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!isExpanded) {
-                    isExpanded = true;
-                    ViewAnimator.animate(container)
-                            .dp()
-                            .width(60f, 250f)
-                            .interpolator(new DecelerateInterpolator())
-                            .duration(800)
-                            .onStop(new AnimationListener.Stop() {
-                                @Override
-                                public void onStop() {
-                                    textview.setVisibility(View.VISIBLE);
-                                    textview.setText("Jakarta Pusat");
-                                    ViewAnimator.animate(textview)
-                                            .dp().translationY(50, 0)
-                                            .alpha(0.1f, 1)
-                                            .singleInterpolator(new OvershootInterpolator())
-                                            .duration(800)
-                                            .start();
-                                }
-                            })
-                            .start();
-                    ViewAnimator
-                            .animate(container).scaleY(0, 1).decelerate().duration(500);
+                if (!container.isLoading()) {
+                    container.setAddress("Jakarta Pusat");
+                    container.extended();
                 } else {
-                    isExpanded = false;
-                    ViewAnimator.animate(textview)
-                            .dp().translationY(0, 50)
-                            .alpha(1, 0.1f)
-                            .singleInterpolator(new OvershootInterpolator())
-                            .duration(800)
-                            .onStart(new AnimationListener.Start() {
-                                @Override
-                                public void onStart() {
-                                    ViewAnimator.animate(container)
-                                            .dp()
-                                            .width(250f, 60f)
-                                            .interpolator(new AccelerateInterpolator())
-                                            .duration(1200)
-                                            .start();
-                                    ViewAnimator
-                                            .animate(container).scaleY(1, 0).accelerate().duration(800);
-
-                                }
-                            })
-                            .onStop(new AnimationListener.Stop() {
-                                @Override
-                                public void onStop() {
-                                    textview.setVisibility(View.GONE);
-                                }
-                            })
-                            .start();
+                    container.collapsed();
                 }
             }
         });
